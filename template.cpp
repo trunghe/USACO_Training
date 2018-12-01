31243a7f27c1be32638b29760baba96eff93d167
trunghe <33113755+trunghe@users.noreply.github.com>
2018-12-01 14:03:12 +0700

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,78 @@
+/*
+ID: trungvt2
+TASK: test
+LANG: C++
+*/
+
+/*
+http://link...
+Description: TODO Write a description
+Category:
+Solution:
+Complexity: TODO Analyze complexity
+Author: Vu Thanh Trung
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+//typedef vector<int> vi;
+//typedef vector<vi> vvi;
+//typedef vector<ll> vl;
+//typedef vector<vl> vvl;
+//typedef vector<vvl> vvvl;
+//typedef pair<int, int> ii;
+//typedef vector<ii> vii;
+//typedef vector<vii> vvii;
+
+#define FOR(i, a, b) \
+    for (int i = (a); i < (b); i++)
+//#define FORE(i, a, b) \
+//    for (int i = (a); i <= (b); i++)
+//#define FORD(i, a, b) \
+//    for (int i = (a); i >= (b); i--)
+
+//#define INF 1e9+7
+//#define INFLL 1e18+7
+//#define esp 1e-9
+//#define PI 3.14159265
+//
+//ll nMod = 1e9 + 7;
+
+//inline ll GCD(ll a, ll b) {while (b != 0) {ll c = a % b; a = b; b = c;} return a;};
+//inline ll LCM(ll a, ll b) {return (a / GCD(a,b)) * b;};
+
+ll max(ll a1, ll b1) {
+    return a1 > b1 ? a1 : b1;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
+
+/* Sample input:
+
+ */
+/* Sample output:
+
+ */
+
+/*
+USER: Trung Vu [trungvt2]
+TASK:
+LANG: C++
+
+Compiling...
+
+ */
